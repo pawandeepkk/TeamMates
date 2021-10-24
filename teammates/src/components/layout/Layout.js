7c66d041fe8ebfe473149227d9f3ed4be2c75391
Pawandeep Kaur <pawandeepk@my.com>
2021-10-24 21:13:04 +0100

    Styled Layout.css

--- teammates/src/components/layout/Layout.js
+++ teammates/src/components/layout/Layout.js
@@ -1,6 +1,7 @@
 import Header from './Header';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import './Layout.css';
 
 function Layout(props) {
     //properties ---------------------
