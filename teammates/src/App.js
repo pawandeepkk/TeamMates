e429c9438731b077ed13f8986555f55719b76c31
Pawandeep Kaur <pawandeepk@my.com>
2021-10-24 21:30:35 +0100

    Created Sign in and contact pages

--- teammates/src/App.js
+++ teammates/src/App.js
@@ -1,5 +1,8 @@
 import Layout from './components/layout/Layout';
 import Home from './components/pages/Home';
+import SignIn from './components/pages/Signin';
+import Contact from './components/pages/Contact';
+
 
 function App() {
   return (
