8a27d7ae7302ea46a7ee61462f378d099869fbfc
Pawandeep Kaur <pawandeepk@my.com>
2021-10-28 13:22:52 +0100

    implemented dummy data using .map method

--- teammates/src/components/pages/Home.js
+++ teammates/src/components/pages/Home.js
@@ -1,39 +1,46 @@
+import React from 'react';
+import { render } from 'react-dom';
 import Card from '../generic/Card';
 import Assignment from './Assignment';
 
-function Home() {
+class Home extends React.Component {
     //dummy data ------------------
-    const UpcomingAssignments = [
-        {
-            title: "Software Development Practice",
-            code: "CI6125",
-            dueDate: "04/11/2021",
-            description: "lorem ipsum",
-        },
-        {
-            title: "Mobile Application Development",
-            code: "CI6330",
-            dueDate: "10/01/2022",
-            description: "lorem ipsum"
-        },
+   render() {
+       this.state = [ {
+        title: "Software Development Practice",
+        code: "CI6125",
+        dueDate: "04/11/2021",
+        description: "lorem ipsum",
+    },
+    {
+        title: "Mobile Application Development",
+        code: "CI6330",
+        dueDate: "10/01/2022",
+        description: "lorem ipsum"
+    },
     ];
+   
     //properties ---------------------
     //hooks ---------------------
     //context ---------------------
     //methods ---------------------
     //view ---------------------
-    return (
-        <section>
-            <h1>My Group Assignments</h1>
-            <Card
-            title="My Upcoming Group Assignments">
-
-            </Card>
-
-        </section>
-
-        
-    )
+        return ( 
+             <section>
+                <h1>My Upcoming Group Assignments</h1>
+                  {this.state.map((assignmentInfo) => {
+                    return(
+                        <Card
+                            title={assignmentInfo.title + " (" + assignmentInfo.code + ")"}>
+                        
+                        </Card>
+                    );
+                   })
+                  }
+                
+             </section>
+         );
+    }
 }
 
 export default Home;
