3a0a5ab9b4d1fcd2b2a8deef2aaf372ce47aad02
Pawandeep Kaur <pawandeepk@my.com>
2021-11-05 17:50:32 +0000

    Added JS function to recover JSON endpoint

--- teammates/src/components/pages/Myprofile.js
+++ teammates/src/components/pages/Myprofile.js
@@ -1,14 +1,35 @@
+import {useState, useEffect} from 'react';
+import {apiRequestWithKey } from '../api/apiRequestWithKey';
+
+const API_URL = "https://my.api.mockaroo.com/";
+const API_KEY = "?key=c4468d80";
+
 function Myprofile() {
     //properties ---------------------
     //hooks ---------------------
+    const [loadingMessage, setLoadingMessage] = useState("Loading records...");
+    const [Modules, setModules] = useState(null);
+
+    useEffect(() => { fetchModules(); }, []);
     //context ---------------------
     //methods ---------------------
+    const fetchModules = async () => {
+        const outcome = await apiRequestWithKey(API_URL, API_KEY, 'Modules');
+        if (outcome.success) setModules(outcome.response);
+        else setLoadingMessage("Modules could not be found.");
+    }
     //view ---------------------
     return (
         <section>
             <h1>My Profile</h1>
+            <h2>All Modules</h2>
+            {
+                !Modules
+                    ? <p>{loadingMessage}</p>
+                    : <Modules list = {Modules}/>
+            }
         </section>
-    )
+    );
 }
 
 export default Myprofile;
